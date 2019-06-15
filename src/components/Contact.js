411cda4bd9513c58d4187e232fd1adff6c01c3f5
hyecheon <rainbow880616@gmail.com>
2019-06-15 13:02:58 +0900

    creating state

---
+++ src/components/Contact.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class Contact extends Component {
+  static propTypes = {
+    contact: PropTypes.object.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    const {name, email, phone} = this.props.contact;
+    return (
+      <div className="card card-body mb-3">
+        <h4>{name}</h4>
+        <ul className="list-group">
+          <li className="list-group-item">email : {email}</li>
+          <li className="list-group-item">phone : {phone}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Contact;
